1667e4b7e75bc13f65719d1aa0c617122ce6aea0
nicocevallos <nicocevallos@localhost>
2023-10-25 16:37:29 -0500

    changed COVID STL to updated data, and part 2

--- part2.m
+++ part2.m
@@ -1,2 +1,16 @@
 load("COVID_STL.mat");
 
+COVID_STL_proportional = COVID_STL/POP_STL;
+
+% Seperating the Delta Varient
+startDelta = find(dates>=datetime("2021-06-30", 'InputFormat', "uuuu-MM-dd"));
+endDelta = find(dates<=datetime("2021-10-26", 'InputFormat','uuuu-MM-dd'));
+datesDelta = dates(startDelta(1):endDelta(end));
+
+
+
+
+%Seperating the Omicron Varient
+startOmicron = find(dates>=datetime("2021-10-27", 'InputFormat', "uuuu-MM-dd"));
+endOmicron = find(dates<=datetime("2022-03-22", 'InputFormat','uuuu-MM-dd'));
+datesOmicron = dates(startOmicron(1):endOmicron(end));
