88741785247a572ef076f2eeb8b0050117a7f3e8
nicocevallos <nicocevallos@localhost>
2023-11-03 05:21:41 -0500

    part 4 done :)

--- part4.m
+++ part4.m
@@ -22,17 +22,15 @@ initial_RV = 0;
 X = [initial_S initial_I initial_R initial_D initial_V initial_BI initial_RV]';     
 
 % These are our paramaters make sure that each group equal to one
-percentInfect = 0.0060;
-otherDeathPercent = 0;
-percentNotInfect = 1 - (percentInfect + otherDeathPercent);
+percentInfect = 0.0065;
+percentOtherDie = 0;
+percentNotInfect = 1 - (percentInfect + percentOtherDie);
 
-percentRecoverNoImmunity = 0.00015;
-percentRecoverImmunity = 0.00015;
-percentInfectDie = 0.00225;
+percentRecoverNoImmunity = percentInfect- 0.005;
+percentRecoverImmunity = 0.003;
+percentInfectDie = 0.002;
 percentStayInfected = 1-(percentRecoverNoImmunity + percentRecoverImmunity + percentInfectDie);
 
-percentRecoverLoseImmunity = 0;
-percentRecoverKeepImmunity = 1;
 
 % We know when the vaccine must have been implemented is before day 100 as
 % that is when breakout infections start to happen at a constant rate. We
@@ -48,13 +46,13 @@ percentRecoverKeepImmunity = 1;
 
 % we can also think about how vaccines reduce the effects of the pandemic.
 % Vaccines mostly effect deaths and stop deaths, that means vaccines roll
-% out when deaths go down so that means that is around day 135.
-firstrunTime = 120; % in days
+% out when deaths go down so that means that is around day 125.
+firstrunTime = 130; % in days
 
 % constructing the matrix
-S_column = [percentNotInfect percentInfect 0 otherDeathPercent 0 0 0]';
+S_column = [percentNotInfect percentInfect 0 percentOtherDie 0 0 0]';
 I_column = [percentRecoverNoImmunity percentStayInfected percentRecoverImmunity percentInfectDie 0 0 0]';
-R_column = [percentRecoverLoseImmunity 0 percentRecoverKeepImmunity 0 0 0 0]';
+R_column = [0 0 1 0 0 0 0]';
 D_column = [0 0 0 1 0 0 0]';
 V_column = [0 0 0 0 0 0 0]'; % this changes per day so for when we are trying to find percent vaccinated per day take the cummulative sum of this
 BI_column = [0 0 0 0 0 0 0]'; % this is what we want to display, to tune this since it is only getting added by the vaccinated, make this look like a smaller version of the infected graph
@@ -83,23 +81,22 @@ vaccineEfficacy = 0.90;
 % we are making it so that breakout infections are only defined as those
 % infections that vaccinated people get, cuz thats the definintion of the
 % term.
-percentInfect = 0.1;
-percentVaccination = 0.001;
+percentInfect = 0.045;
+percentVaccination = 0.00375;
 percentNotInfect = 1 - (percentInfect + percentVaccination);
 
-percentRecoverNoImmunity = 0.20;
-percentRecoverImmunity = 0.0001;
-percentInfectDie = 0.00004;
-% percentInfected to Vaccinated is always zero as infected people dont get vaccinated until they recover
+percentRecoverNoImmunity = 0.25;
+percentRecoverImmunity = 0.2;
+percentInfectDie = 0.00245;
 percentStayInfected = 1-(percentRecoverNoImmunity + percentRecoverImmunity + percentInfectDie);
 
 percentRecoverKeepImmunity = 1 - (percentVaccination);
 
-percentBreakthroughInfection = percentInfect*(vaccineEfficacy);
+percentBreakthroughInfection = (percentInfect - 0.01)*(vaccineEfficacy - 0.2);
 percentStayVaccinated = 1-percentBreakthroughInfection;
 
 percentDieBI = percentInfectDie*(1-vaccineEfficacy);
-percentRecoverVaccineNoImmunity = (percentRecoverNoImmunity)*(vaccineEfficacy);
+percentRecoverVaccineNoImmunity = (percentRecoverNoImmunity - 0.2)*(vaccineEfficacy);
 percentRecoverVaccineImmunity = (percentRecoverImmunity)*(1+vaccineEfficacy);
 percentStayBI = 1 - (percentDieBI + percentRecoverVaccineNoImmunity + percentRecoverVaccineImmunity);
 
@@ -131,6 +128,8 @@ for t = firstrunTime:secondrunTime+1
     new_I(t) = getNew(I_row, X(:, t-1)) + (BI_row(5) * X(5, t-1)); % adding the vaccination to breakthrough infection pipeline
 end
 
+
+%% these are the graphs for the part 4
 figure
 hold on
 plot(X(1, :));
@@ -154,7 +153,8 @@ plot(new_I)
 ylabel("percent of population")
 xlabel("days")
 title("Mock COVID Data")
-legend("new infections per day", "new Infections from model per day")
+lgd = legend("new infections per day", "new Infections from model per day");
+lgd.Location = 'best';
 
 figure
 hold on
@@ -163,7 +163,21 @@ plot(X(4, :))
 ylabel("percent of population")
 xlabel("days")
 title("Mock COVID Data")
-legend("Cumulative Deaths", "deaths from model")
+lgd = legend("Cumulative Deaths", "deaths from model");
+lgd.Location = 'best';
+
+figure
+plot(X(5, :) + X(6, :) + X(7, :));
+ylabel("percent of population")
+xlabel("days")
+title("Total People Vaccinated")
+
+figure
+plot(X(6, :));
+ylabel("percent of population")
+xlabel("days")
+title("People expirencing Break Through infections")
+
 %% Functions that are used in this part
 function f = getNew(row, column)
     f = row(1) * column(1);
